f1d482e7d8d2df960526f616914769a4a9806b83
ahmadbilal-kh <ahmad.bilal.kh046@gmail.com>
2025-04-17 10:47:41 +0500

    create pakege and navbar component

---
+++ src/components/Navbar.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+      <nav class="navbar">
+      <div class="nav-left">
+        <a href="/" class="name-btn"><i>Ahmad Bilal</i></a>
+      </div>
+      <div class="nav-right">
+        <a href="/">Home</a>
+        <a href="#about">About</a>
+        <a href="#skills">Skills</a>
+        <a href="#eductaion">Education</a>
+        <a href="#projects">Projects</a>
+        <a href="#services">Services</a>
+        <a href="#contact">Contact</a>
+      </div>
+    </nav>
+    </div>
+  )
+}
+
+export default Navbar
