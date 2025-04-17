1e09f07633e5f57c27b87e29c56a4234ad216ee2
ahmadbilal-kh <ahmad.bilal.kh046@gmail.com>
2025-04-17 11:22:40 +0500

    add About section

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,24 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
   return (
     <div>
-      About
+      <div className="section-glow-end"></div>
+      <div className="about" id="about">
+        <h2 className="section-title">About</h2>
+        <p className="about-text">
+          I'm <span className="glow-name">Ahmad Bilal</span>, a passionate Web
+          Developer with over 1.5 years of hands-on experience crafting modern,
+          responsive, and dynamic web applications. My skillset spans across
+          HTML, CSS, JavaScript, React, Node.js, Next.js, Redux, MongoDB, PHP,
+          and WordPress. I love turning ideas into real-life products that are
+          not only visually appealing but also user-friendly and high
+          performing. I'm always eager to learn, collaborate, and contribute to
+          projects that make a difference.
+        </p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default About
-
+export default About;
