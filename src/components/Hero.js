c6bf37d957cfc38fca60d1121a76e41c070cab13
ahmadbilal-kh <ahmad.bilal.kh046@gmail.com>
2025-04-17 11:18:04 +0500

    add images and create hero component

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,7 +1,27 @@
 import React from "react";
+import Image from "../components/images/ahmad.jpg";
 
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <div>
+      <section className="hero">
+        <div className="hero-left">
+          <h1>
+            Hi, I'm <span className="glow-name">Ahmad Bilal</span>
+          </h1>
+
+          <p>I'm a passinate Web Developer. Lets build something together!</p>
+          <button className="btn">
+            <a href="#contact">Contact Me</a>
+          </button>
+        </div>
+
+        <div className="hero-right">
+          <img src={Image} alt="" />
+        </div>
+      </section>
+    </div>
+  );
 };
 
 export default Hero;
